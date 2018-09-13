ea7fed5da95fa59877a1e6205154828d93c3ea84
Sean Trane <seantrane@users.noreply.github.com>
2018-09-13 14:34:07 +0530

    fix: update default-enabled casks

--- osx/install-casks.sh
+++ osx/install-casks.sh
@@ -23,7 +23,7 @@ if type "brew" &> /dev/null; then
     # brew cask install adobe-photoshop-lightroom
     # brew cask install adobe-reader
     brew cask install alfred
-    brew cask install atom
+    # brew cask install atom
     # brew cask install audacity
     # brew cask install citrix-receiver
     # brew cask install cleanmymac
@@ -36,41 +36,40 @@ if type "brew" &> /dev/null; then
     # brew cask install evernote
     brew cask install filezilla
     # brew cask install firefox
-    # brew cask install flash
-    brew cask install gimp
+    # brew cask install gimp
     # brew cask install github
     # brew cask install gitx
     brew cask install google-chrome
     brew cask install handbrake
     brew cask install imagealpha
     brew cask install imageoptim
-    brew cask install intellij-idea-ce
+    # brew cask install intellij-idea-ce
     brew cask install iterm2
     brew cask install java
     # brew cask install jenkins
     # brew cask install jenkins-lts
     # brew cask install jenkins-menu
     # brew cask install kaleidoscope-beta
-    brew cask install keka
+    # brew cask install keka
+    # brew cask install libreoffice
     # brew cask install little-snitch
     # brew cask install mamp
     # brew cask install microsoft-lync
     # brew cask install microsoft-office
-    # brew cask install openoffice
     # brew cask install phpstorm
-    brew cask install robomongo
+    # brew cask install robomongo
     # brew cask install screenflow
-    brew cask install sequel-pro
+    # brew cask install sequel-pro
     brew cask install slack
     brew cask install sourcetree
-    brew cask install spectacle
+    # brew cask install spectacle
     # brew cask install stellarium
-    brew cask install sublime-text
-    brew cask install svnx
+    # brew cask install sublime-text
+    # brew cask install svnx
     # brew cask install textexpander
-    # brew cask install the-unarchiver
+    brew cask install the-unarchiver
     # brew cask install tower
-    brew cask install vagrant
+    # brew cask install vagrant
     brew cask install virtualbox
     brew cask install visual-studio-code
     # brew cask install vitalsource-bookshelf
