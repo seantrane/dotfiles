98c112e370b657b210316aac5c0fe47fa77e9c81
Sean Trane <seantrane@users.noreply.github.com>
2018-09-14 11:54:59 +0530

    fix: Tilde does not expand in quotes. Use $HOME. [SC2088]

--- osx/set-prefs.sh
+++ osx/set-prefs.sh
@@ -57,7 +57,7 @@ fi
 # Remove local update-agent
 if [ -f "$GOOGLE_UPDATER_FILE" ];
 then
-    sudo "~/$GOOGLE_UPDATER_FILE" --uninstall
+    sudo "$HOME/$GOOGLE_UPDATER_FILE" --uninstall
 fi
 
 # Void the update-checker
@@ -69,7 +69,7 @@ defaults write com.google.Keystone.Agent checkInterval 0
 ###############################################################################
 
 SUBLIME_DOT_PREFS="$HOME/.dotfiles/osx/init/Sublime"
-SUBLIME_PREF_PATH="~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/"
+SUBLIME_PREF_PATH="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/"
 
 if [ -d "$SUBLIME_PREF_PATH" ];
 then
