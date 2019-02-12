7cbf164d8a3a4f0dc8580c49b101d3648b130e1a
Sean Trane <seantrane@users.noreply.github.com>
2019-02-12 06:47:17 -0500

    fix: resolve some shellcheck warnings

--- rails/install.sh
+++ rails/install.sh
@@ -10,23 +10,23 @@ if ! type "rails" &> /dev/null; then
 
     # Ensure Ruby/rbenv is installed and gem/rbenv commands are available.
     if ! type "gem" &> /dev/null || ! type "rbenv" &> /dev/null; then
-        source $DOTFILES/ruby/install.sh
+        . "$DOTFILES/ruby/install.sh"
     fi
 
     # Ensure Node.js is installed.
-    ! type "node" &> /dev/null && source $DOTFILES/node/install.sh
+    ! type "node" &> /dev/null && . "$DOTFILES/node/install.sh"
 
     if type "gem" &> /dev/null; then
-        source $DOTFILES/ruby/gem-update.sh
+        . "$DOTFILES/ruby/gem-update.sh"
         sh_info "Installing Ruby On Rails..."
         gem install rails -v 4.2.4
     fi
 
-    source $DOTFILES/ruby/rehash.sh
+    . "$DOTFILES/ruby/rehash.sh"
 fi
 
 if type "rails" &> /dev/null; then
-    sh_success "$(rails --version) installed: $(which rails)\n"
+    sh_success "$(rails --version) installed: $(command -v rails)\\n"
     gem which rails
     echo ""
 fi
--- serverless/completion.zsh
+++ serverless/completion.zsh
@@ -2,7 +2,9 @@
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
+# shellcheck disable=SC1090
 [[ -f "$(brew --prefix)/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh" ]] && . "$(brew --prefix)/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh"
 # tabtab source for sls package
 # uninstall by removing these lines or running `tabtab uninstall sls`
+# shellcheck disable=SC1090
 [[ -f "$(brew --prefix)/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh" ]] && . "$(brew --prefix)/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh"
