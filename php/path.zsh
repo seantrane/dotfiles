48984466bfed96cef70d4a58042266045091df6f
Sean Trane <seantrane@users.noreply.github.com>
2019-02-20 21:18:54 -0500

    fix: remove old/unused stuff

--- php/path.zsh
+++ php/path.zsh
@@ -4,3 +4,8 @@
 if type "brew" &> /dev/null && [ "$(brew ls --versions php56)" ]; then
     export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
 fi
+
+# php70 support:
+if type "brew" &> /dev/null && [ "$(brew ls --versions php70)" ]; then
+    export PATH="$(brew --prefix homebrew/php/php70)/bin:$PATH"
+fi
