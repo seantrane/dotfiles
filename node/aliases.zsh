a37ca812e3c11ad851d18d4bc572aaee2e453375
Sean Trane Sciarrone <seantrane@users.noreply.github.com>
2022-05-05 22:18:35 -0400

    fix: improve node install, add npm list aliases

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -4,3 +4,6 @@
 
 alias node-update="brew update node"
 alias npm-update="(cd; npm cache clean -g -f; node-update; npm install -g npm@latest; npm update -g; npm cache clean -g -f)"
+
+alias npmlistg="npm list -g --depth=0 2>/dev/null"
+alias npmlist="npm list --depth=0 2>/dev/null"
