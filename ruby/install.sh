01072ac75f65371fdcc891ef3a7b945b4dc56f57
Sean Trane Sciarrone <seantrane@users.noreply.github.com>
2022-05-05 22:30:35 -0400

    fix: update ruby install, paths

--- ruby/install.sh
+++ ruby/install.sh
@@ -11,6 +11,8 @@ if type "apt-get" &> /dev/null; then
     sudo apt-get update
     sh_info "Installing some dependencies for Ruby..."
     sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+elif type "brew" &> /dev/null; then
+    brew install ruby
 fi
 
 # Install Ruby-environment (rbenv)
--- ruby/path.zsh
+++ ruby/path.zsh
@@ -2,7 +2,31 @@
 #
 # Add PATH for gem executables
 
+# If you need to have openssl@1.1 first in your PATH run:
+# [[ -d "/usr/local/opt/openssl@1.1/bin" ]] && export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+# For compilers to find openssl@1.1 you may need to set:
+# [[ -d "/usr/local/opt/openssl@1.1/lib" ]] && export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+# [[ -d "/usr/local/opt/openssl@1.1/include" ]] && export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+# For pkg-config to find openssl@1.1 you may need to set:
+# [[ -d "/usr/local/opt/openssl@1.1/lib/pkgconfig" ]] && export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
+
+# If you need to have ruby first in your PATH run:
+[[ -d "/usr/local/opt/ruby/bin" ]] && export PATH="/usr/local/opt/ruby/bin:$PATH"
+# For compilers to find ruby you may need to set:
+[[ -d "/usr/local/opt/ruby/lib" ]] && export LDFLAGS="-L/usr/local/opt/ruby/lib"
+[[ -d "/usr/local/opt/ruby/include" ]] && export CPPFLAGS="-I/usr/local/opt/ruby/include"
+# For pkg-config to find ruby you may need to set:
+[[ -d "/usr/local/opt/ruby/lib/pkgconfig" ]] && export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+
+# By default, binaries installed by gem will be placed into:
+[[ -d "/usr/local/lib/ruby/gems/2.3.0/bin" ]] && export PATH="/usr/local/lib/ruby/gems/2.3.0/bin:$PATH"
+[[ -d "/usr/local/lib/ruby/gems/2.4.0/bin" ]] && export PATH="/usr/local/lib/ruby/gems/2.4.0/bin:$PATH"
+[[ -d "/usr/local/lib/ruby/gems/2.5.0/bin" ]] && export PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
+[[ -d "/usr/local/lib/ruby/gems/2.6.0/bin" ]] && export PATH="/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+[[ -d "/usr/local/lib/ruby/gems/3.1.0/bin" ]] && export PATH="/usr/local/lib/ruby/gems/3.1.0/bin:$PATH"
+
 [[ -d "$HOME/.gem/ruby/2.3.0/bin" ]] && export PATH="$HOME/.gem/ruby/2.3.0/bin:$PATH"
 [[ -d "$HOME/.gem/ruby/2.4.0/bin" ]] && export PATH="$HOME/.gem/ruby/2.4.0/bin:$PATH"
 [[ -d "$HOME/.gem/ruby/2.5.0/bin" ]] && export PATH="$HOME/.gem/ruby/2.5.0/bin:$PATH"
 [[ -d "$HOME/.gem/ruby/2.6.0/bin" ]] && export PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
+[[ -d "$HOME/.gem/ruby/3.1.0/bin" ]] && export PATH="$HOME/.gem/ruby/3.1.0/bin:$PATH"
