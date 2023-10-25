27f3ddb644ddba8d1bcea4de46a5e0f6fab44c0f
seantrane <seantrane@users.noreply.github.com>
2023-10-25 04:43:36 -0400

    fix: remove redundant path files

--- elixir/path.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if type "elixir" &> /dev/null; then
-    export MANPATH="$(brew --prefix erlang)/lib/erlang/man:$MANPATH"
-fi
--- terraform/path.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -d "$HOME/terraform" ]] && export PATH="$HOME/terraform:$PATH"
-
-[[ -d "/$(brew --prefix terraform)/bin" ]] && export PATH="/$(brew --prefix terraform)/bin:$PATH"
