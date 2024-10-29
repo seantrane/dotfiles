d95e7c3d4cdaee4685ef51e53a9d850ef0fad866
Sean Trane Sciarrone <seantrane@users.noreply.github.com>
2024-10-29 06:38:52 -0400

    style: add eslint.config.js file

---
+++ .github/eslint.config.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+    },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    ignorePatterns: ['!.*', '.devcontainer/*', '.vscode/*', '**/node_modules/.*'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+  },
+];
